6c73c865c3dadfa94e838b2f6b70fa5c98e98b4c
Chiluka Sharanya <chiluka.sharanya@gmail.com>
2018-01-12 11:09:53 +0530

    handling react components in login page

---
+++ login/src/App.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import bg from './bg.jpg';
+import './index.css';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		  name: "",
+		  password: "",
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handlePasswordChange=this.handlePasswordChange.bind(this);
+	}
+	
+	handleSubmit(event) {
+        event.preventDefault();
+		this.props.handleSubmit(this.state.name);
+		this.props.handleSubmit(this.state.password);
+		
+    }
+
+    handleNameChange(event) {
+        this.setState({
+            name: event.target.value,
+		});
+	}
+
+	handlePasswordChange(event) {
+        this.setState({
+            password: event.target.value,
+		});
+	}
+
+  render() {
+    return (
+      <div className="main-agileinfo slider ">
+		<div className="items-group">
+			<div className="item agileits-w3layouts">
+				<div className="block text main-agileits"> 
+					<span className="circleLight"></span> 
+					
+					<div className="login-form loginw3-agile"> 
+						<div className="agile-row">
+							<h1>Login Form</h1> 
+							<div className="login-agileits-top"> 	
+								<form onSubmit={this.handleSubmit}> 
+									<p>User Name </p>
+									<input type="text" 
+												 className="name" 
+												 id="name" 
+												 required=""
+												 onChange={this.handleNameChange}
+                         value={this.state.name} />
+									<p>Password</p>
+									<input type="password" 
+												 className="password" 
+												 id="password"
+												 required=""
+												 onChange={this.handlePasswordChange}
+												 value={this.state.password}/>  
+									<label className="anim">
+										<input type="checkbox" className="checkbox"/>
+										<span> Remember me ?</span> 
+									</label>   
+									<input type="submit" value="Login"/> 
+								</form> 	
+							</div> 
+							<div className="login-agileits-bottom wthree"> 
+								<h6><a href="#">Forgot password?</a></h6>
+							</div> 
+						</div>  
+					</div>   
+				</div> 
+			</div>   
+		</div>
+	</div>	 
+    );
+  }
+}
+
+export default App;
