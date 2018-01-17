4457e337ba899e1877a2cb6c68111965cd88c9c9
susmita2162 <susmitamadineni@gmail.com>
2018-01-17 14:44:45 +0530

    final fields

--- signup/src/App.jsx
+++ signup/src/App.jsx
@@ -8,12 +8,16 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-		  name: "",
-		  password: "",
+			name: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
 		};
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleNameChange = this.handleNameChange.bind(this);
+		this.handleEmailChange = this.handleEmailChange.bind(this);
 		this.handlePasswordChange=this.handlePasswordChange.bind(this);
+		this.handleConfirmPasswordChange=this.handleConfirmPasswordChange.bind(this);
 	}
 	
 	handleSubmit(event) {
@@ -21,7 +25,7 @@ class App extends React.Component {
 		
 		request
 		.post("http://localhost:9000/users")
-		.send({name: this.state.name ,password: this.state.password})
+		.send({name: this.state.name ,email: this.state.email ,password: this.state.password,confirmPassword: this.state.confirmPassword})
 		.then(
 		  (response) => {
 			// response.body will be the returned data from your play app, which is an array of objects
@@ -38,11 +42,27 @@ class App extends React.Component {
 		});
 	}
 
+	handleEmailChange(event) {
+		this.setState({
+				email: event.target.value,
+});
+}
+
 	handlePasswordChange(event) {
         this.setState({
             password: event.target.value,
 		});
 	}
+
+	handleConfirmPasswordChange(event) {
+		this.setState({
+				confirmPassword: event.target.value,
+				
+});
+}
+
+
+
 
   render() {
     return (
@@ -64,22 +84,36 @@ class App extends React.Component {
 												 required=""
 												 onChange={this.handleNameChange}
                          value={this.state.name} />
+									<p>email id </p>
+									<input type="text" 
+												 className="email" 
+												 id="email" 
+												 required=""
+												 onChange={this.handleEmailChange}
+                         value={this.state.email} />
 									<p>Password</p>
 									<input type="password" 
 												 className="password" 
 												 id="password"
 												 required=""
 												 onChange={this.handlePasswordChange}
-												 value={this.state.password}/>  
+												 value={this.state.password}/> 
+									<p>Confirm Password</p> 
+									<input type="password" 
+												 className="confirmpassword" 
+												 id="confirmpassword"
+												 required=""
+												 onChange={this.handleConfirmPasswordChange}
+												 value={this.state.confirmPassword}/>  
 									<label className="anim">
 										<input type="checkbox" className="checkbox"/>
 										<span> Remember me ?</span> 
 									</label>   
-									<input type="submit" value="Login"/> 
+									<input type="submit" value="Sign up"/> 
 								</form> 	
 							</div> 
 							<div className="login-agileits-bottom wthree"> 
-								<h6><a href="#">Forgot password?</a></h6>
+								<h6><a href="#">Already a user? Login</a></h6>
 							</div> 
 						</div>  
 					</div>   
