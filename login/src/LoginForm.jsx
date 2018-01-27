b1eac32ff7abba1759f9ac5c4616033c857b78ae
Chiluka Sharanya <chiluka.sharanya@gmail.com>
2018-01-27 12:57:00 +0530

    changes made

--- login/src/LoginForm.jsx
+++ login/src/LoginForm.jsx
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import './App.css';
-import bg from './bg.jpeg';
-import grid from './grid.png';
+
+import grid from './grid.png'; 
+
+import './LoginForm.css';
 import request from 'superagent';
 
 
-class App extends React.Component {
+class LoginForm extends React.Component {
 	
 
 	render() {
 		return (
 			<body>
-      <h1 class="wthree">online food world</h1>
+      <h1 class="wthree">PLATROVA</h1>
 	<div class="container login-section">	
 		<div class="login-w3l">	
 			<div class="login-form">	
 				<h2 class="wthree">LOGIN</h2>	
+				
 				<form action="#" method="post" class="agile_form">
-					<div class="w3ls-name1">
-						<label class="header">name</label>
-						<input placeholder="loremipsum " name="first name" type="text" required=""/>
-					</div>	
+						
 					<div class="w3ls-name1">
 						<label class="header">email</label>
 						<input placeholder="email@example.com" name="Email" type="email" required=""/>
@@ -33,9 +32,9 @@ class App extends React.Component {
 				</form>
 			</div>
 		</div>	
-		<div class="login-w3l-bg">	
-			<img src="grid.png" alt=""/>
-		</div>	 
+			
+		<div class="login-w3l-bg">
+		 <img src="grid.png"/></div>
 		<div class="clearfix"></div>	
 	</div> 	
 	
@@ -46,4 +45,4 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+export default LoginForm;
--- login/src/index.js
+++ login/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import LoginForm from './LoginForm';
+
+ReactDOM.render(<LoginForm/>, document.getElementById('root'));
 registerServiceWorker();
