ea7158a909da611e1ac49624dcb4d274a466fbdd
susmita2162 <susmitamadineni@gmail.com>
2018-02-03 15:56:48 +0530

    Add files via upload

--- platrova/src/Search.jsx
+++ platrova/src/Search.jsx
@@ -2,51 +2,46 @@ import React, { Component } from 'react';
 
 import slide1 from './slide1.jpg';
 import request from 'superagent';
+
 import './Search.css';
 
-import { Button } from 'react-bootstrap';
+import { Button ,Form, Image} from 'react-bootstrap';
+
+
 
 class Search extends React.Component {
 
     render() {
-
+              
         return (
-            <body>
 
-            <div>
 
-                <div class="container">
-                    <div class="col-lg-8" middle>
-                    <div class="input-group">
-                    
-                  
-                    
-                        <input type="text" class="form-control"  placeholder="Search for..." name="search" />
-                        <div class = "input-group-btn">
-                        <Button bsStyle="danger" bsSize ="middle" href = "#" active >
+                            
+                    <div class="container">
+                        <img src={slide1} alt="slide1" responsive/>
+                            <div class="col-lg-7" class="col-lg-8">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Search for..." name="search" />
+                           
+                           </div>
+                           
+                           
+                           
+                           <div class="col-lg-8">
+                                    <div class="btn btn-block">
+                                    <Button  bsStyle="danger" bsSize="medium float-right "href="#" active >
+                                        <span class="glyphicon glyphicon-search"></span>Search
+                                    </Button>
+                                </div>
                         
-                        <span class="glyphicon glyphicon-search"></span>             Search
-                        
-                        
-                        </Button>
-
-                        
-                        </div>
-                        </div>
-                        </div>
                        
-                    </div>
-                </div>
-                </body>
-          
-        );
-
-
+                     </div>
+                   </div>
+                   
+            </div>
+                
+                       );
     }
-
-
-
-
 }
 
 export default Search;
