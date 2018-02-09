f984d27c044c42b331425e32209e16d08dff3402
Chiluka Sharanya <chiluka.sharanya@gmail.com>
2018-02-09 09:55:51 +0530

    trying with hovereffect instead of carousel

--- platrova/src/Home.jsx
+++ platrova/src/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Search from './Search';
 import CarouselEffect from './CarouselEffect';
+import HoverEffect from './HoverEffect';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
              <Search />
-             <CarouselEffect />
+             <HoverEffect />
              </div>
         );
     }
