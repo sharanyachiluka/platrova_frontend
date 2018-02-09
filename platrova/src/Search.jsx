abcdcc012d44b0e2073134a20b0244d2e01a554d
Chiluka Sharanya <chiluka.sharanya@gmail.com>
2018-02-09 10:09:20 +0530

    removed  background image of search component

--- platrova/src/Search.jsx
+++ platrova/src/Search.jsx
@@ -5,42 +5,31 @@ import request from 'superagent';
 
 import './Search.css';
 
-import { Button ,Form, Image} from 'react-bootstrap';
+import { Button, Form, Image } from 'react-bootstrap';
 
 
 
 class Search extends React.Component {
 
     render() {
-              
+
         return (
-
-
-                            
-                    <div class="container">
-                        <img src={slide1} alt="slide1" responsive/>
-                            <div class="col-lg-7" class="col-lg-8">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="Search for..." name="search" />
-                           
-                           </div>
-                           
-                           
-                           
-                           <div class="col-lg-8">
-                                    <div class="btn btn-block">
-                                    <Button  bsStyle="danger" bsSize="medium float-right "href="#" active >
-                                        <span class="glyphicon glyphicon-search"></span>Search
+            <div class="container">
+                <div class="col-lg-9" class="col-lg-10">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Search for..." name="search" />
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="btn btn-block">
+                            <Button bsStyle="danger" bsSize="medium float-right " href="#" active >
+                                <span class="glyphicon glyphicon-search"></span>Search
                                     </Button>
-                                </div>
-                        
-                       
-                     </div>
-                   </div>
-                   
+                        </div>
+                    </div>
+                </div>
             </div>
-                
-                       );
+
+        );
     }
 }
 
