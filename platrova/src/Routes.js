09de9e8fbf7918052ba0732c5cd1cce5cc597966
susmita2162 <susmitamadineni@gmail.com>
2018-02-21 13:42:24 +0530

    working on client

--- platrova/src/Routes.js
+++ platrova/src/Routes.js
@@ -6,7 +6,7 @@ import App from './App';
 import Header from './Header';
 import Footer from './Footer';
 import { BrowserRouter,Route,Switch} from 'react-router-dom';
-import Client from './Client';
+import ClientHomepage from './ClientHomepage';
 
 class Routes extends React.Component {
     render() {
@@ -17,7 +17,7 @@ class Routes extends React.Component {
             <Route  path="/"  exact component={Home} />
             <Route  path="/signup"  exact component={SignUp} />
             <Route  path="/login"    exact component={LoginForm} />
-            <Route path="/createres" exact component={Client} />
+            <Route path="/createres" exact component={ClientHomepage} />
             <Footer />
             </div>
             
