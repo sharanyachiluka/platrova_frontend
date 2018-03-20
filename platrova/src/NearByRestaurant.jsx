d755f16400b78b78b8861cbb06e79142352b20c1
Chiluka Sharanya <chiluka.sharanya@gmail.com>
2018-03-20 16:11:54 +0530

    working on result alignment

--- platrova/src/NearByRestaurant.jsx
+++ platrova/src/NearByRestaurant.jsx
@@ -3,25 +3,33 @@ import { Card, CardImg, CardText,
     CardTitle, CardSubtitle,Row,Col,CardBody } from 'reactstrap';
     import './SearchRestaurant.css';
 
+    var divStyle = {
+        align : "right",
+    }
     class NearByRestaurant extends React.Component {
         render () {
             return (
-              <Row>
+              <Col sm="4">
               <div class="box">
-                <p><img src={this.props.img} height="200" width="200"></img>&nbsp;&nbsp;
-                <h1>{this.props.name}</h1>
-                   <h4>{this.props.type}
-                    {this.props.cuisine}</h4></p>
-                <p>Area:{this.props.area}</p>
+              <div class="clearfix">
+             <img src={this.props.img} height="200" width="300"></img>
+              &nbsp;&nbsp;  {this.props.name}<br/>
+               Type: {this.props.type}<br/>
+                Cuisine: {this.props.cuisine}<br/>
+                    Distance: {this.props.distance}km<br/>
+                Area: {this.props.area}</div><br/>
+             <details>
+                 <summary><button type="button" class="btn btn-info">More Info</button></summary>
                 <p>Address:{this.props.address}</p>
                <p>Contact:{this.props.contact}</p>
                <p>Timings:{this.props.timings}</p>
                <p>Homepage url:{this.props.hpUrl}</p>
                <p>Facebookpage url:{this.props.fbUrl}</p>
                <p>Cost:{this.props.cost}</p> 
-               <p>Distance:{this.props.distance}km</p>          
+         </details> 
+                
               </div>
-              </Row>
+              </Col>
             );
         }
     }
